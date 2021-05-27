6022d5ce101f7fe8135d1cbd2d1ad0dd30559a18
democvidev <cnvvc_vlad@mail.ru>
2021-05-27 22:25:59 +0200

    add phone numbers, buttons, update

--- CoursPHP/amazin.php
+++ CoursPHP/amazin.php
@@ -32,7 +32,7 @@
             <p>Commencez le questionnaire : <a href="?step=1" role="button" class="btn btn-success">Commencer</a></p>
             
         <?php
-        } elseif ($_GET['step'] === '1') { ?>
+        } elseif (isset($_GET['step']) && $_GET['step'] === '1') { ?>
 
             <!-- Etape 1 : A afficher uniquement une fois que le boutton "Commencer" a été pressé -->
             <h2>Question 1</h2>
@@ -42,7 +42,7 @@
             <a href="?step=2&count=1" role="button" class="btn btn-secondary">sans avis</a> <!-- rapporte 1 point -->
 
         <?php
-        } elseif ($_GET['step'] === '2') {
+        } elseif (isset($_GET['step']) && $_GET['step'] === '2') {
             $q1 = $_GET['count']; ?>
 
             <!-- Etape 2 : A afficher uniquement une fois que l'étape 1 a été résolue -->
@@ -53,50 +53,54 @@
             <a href="?step=3&count=<?= $q1 + 1?>" role="button" class="btn btn-secondary">sans avis</a> <!-- rapporte 1 point -->
             
         <?php
-        } elseif ($_GET['step'] === '3') {
+        } elseif (isset($_GET['step']) && $_GET['step'] === '3') {
             $q2 = $_GET['count']; ?>
 
             <!-- Etape 3 : A afficher uniquement une fois que l'étape 2 a été résolue -->
             <h2>Question 3</h2>
             <p>L'agent a-t-il résolu votre problème ?</p>
             <a href="?step=5&count=<?= $q2 + 1?>" role="button" class="btn btn-success">oui</a> <!-- rapporte 1 point -->
-            <a href="?step=4&count=<?= $q2 - 1?>" role="button" class="btn btn-danger">non</a> <!-- rapporte -1 point -->
+            <a href="?step=4&count=<?= $q2 - 1?>&nb=" role="button" class="btn btn-danger">non</a> <!-- rapporte -1 point -->
 
         <?php
-        } elseif ($_GET['step'] === '4') {
-            $count = $_GET['count']; ?>
+        } elseif (isset($_GET['step']) && $_GET['step'] === '4') {
+            if (isset($_GET['count'])) {
+                $count = $_GET['count'];
+            } ?>
 
             <!-- Etape 4 : A afficher uniquement si "non" a été répondu à l'étape 3 -->
             <p>Votre problème n'a pas été résolu.</p>
             <p>Pour être rappelé, entrez votre numéro de téléphone dans le clavier virtuel et validez :</p>
             <!-- Coder ici un clavier numérique permettant de saisir le numéro de téléphone -->
-            <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
-                <div class="input-group">
-                    <div class="input-group-text" id="btnGroupAddon">tel</div>
-                    <input type="tel" class="form-control" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="Input group example" aria-label="Input group example" aria-describedby="btnGroupAddon" required>
-                </div>
-                <div class="btn-group me-2" role="group" aria-label="First group">
-                    <a href="" type="button" class="btn btn-primary">1</a>                   
-                    <a href="" type="button" class="btn btn-primary">2</a>
-                    <a href="" type="button" class="btn btn-primary">3</a>
-                    <a href="" type="button" class="btn btn-primary">4</a>
-                    <a href="" type="button" class="btn btn-primary">5</a>
-                    <a href="" type="button" class="btn btn-primary">6</a>
-                    <a href="" type="button" class="btn btn-primary">7</a>
-                    <a href="" type="button" class="btn btn-primary">8</a>
-                    <a href="" type="button" class="btn btn-primary">9</a>
-                </div>                
-            </div>
+            <form action="" method="get">
+                <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+                    <div class="input-group">
+                        <div class="input-group-text" id="btnGroupAddon">tel</div>
+                        <input type="tel" value="<?= $_GET['nb']?>" class="form-control" pattern="0[0-7]{1}[0-9]{8}" placeholder="" aria-label="Input group example" aria-describedby="btnGroupAddon" maxLength="10" required>
+                    </div>
+                    <div class="btn-group me-2" role="group" aria-label="First group">
 
-            <!-- Afficher ici le numéro de téléphone qui s'affiche au fur et à mesure de la saisie-->
-            <p>Votre numéro : #numéro de téléphone saisi#</p>
-            <a href="" role="button" class="btn btn-success">Valider</a> <!-- Validation du numéro de téléphone -->  
-            <a href="?step=5&count=<?= $count ?>" role="button" class="btn btn-warning">Ne pas être rappelé</a>            
+                        <?php for ($i = 0; $i <= 9; $i++) { ?>
+
+                            <a href="?step=4&count=<?=$count ?>&nb=<?= $_GET['nb'] . $i?>" type="button" class="btn btn-primary"><?= $i?></a>   
+
+                        <?php } ?>
+
+                        <a href="?step=4&count=<?=$count ?>&nb=<?= substr($_GET['nb'], 0, -1) ?>" role="button" class="btn btn-warning">Correction</a>
+                        <a href="?step=4&count=<?=$count ?>&nb=" role="button" class="btn btn-success">Réinitialisation</a>
+                    </div>                
+                </div>
+                
+                <!-- Afficher ici le numéro de téléphone qui s'affiche au fur et à mesure de la saisie-->
+                <p>Votre numéro : #numéro de téléphone saisi#</p>
+                <a href="" role="button" class="btn btn-success">Valider</a> <!-- Validation du numéro de téléphone -->  
+                <a href="?step=5&count=<?= $count ?>" role="button" class="btn btn-warning">Ne pas être rappelé</a>            
+            </form>
 
             <!-- Si un téléphone à été saisi, afficher "Vous serez rappelé très prochainement au #numéro de téléphone#" -->
 
         <?php
-        } else {
+        } elseif (isset($_GET['count'])) {
             $count = $_GET['count']; ?>
 
             <!-- Etape finale : A afficher si "oui" a été répondu à la question 3 ou si l'étape 4 a été résolue -->
