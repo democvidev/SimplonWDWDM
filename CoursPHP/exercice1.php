bc591046a46a825772a65a099b621ff6ac33aef0
democvidev <cnvvc_vlad@mail.ru>
2021-05-27 14:14:41 +0200

    add file

---
+++ CoursPHP/exercice1.php
@@ -0,0 +1,6 @@
+<?php
+// Exercice : Ecrire une fonction qui supprime les espaces et met la phrase en camelCase
+$sentence = "le chat est mort"; //devient : leChatEstMort
+// indice : si $mot = "jouet" alors $mot[0] = "j", $mot[1] = "o", ...
+// indice : chercher sur google une fonction qui renvoie la longueur d'une chaine de caractères (nombre de caractère)
+
