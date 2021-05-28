8a449ee4c12f661734a7c164ec8c0fcc18400ad4
democvidev <cnvvc_vlad@mail.ru>
2021-05-28 16:07:10 +0200

    updated files

--- ProjectForm/index.php
+++ ProjectForm/index.php
@@ -10,26 +10,15 @@ if ($_POST === []) {
 
 
 if ($_POST != null) {
-    if (isset($_POST['last_name']) && isset($_POST['first_name']) && isset($_POST['tel_number']) && isset($_POST['date_achat']) && isset($_POST['question1']) && isset($_POST['question2']) && isset($_POST['question3']) && $_POST['question3'] == '1') {
-        $count = $_POST['question1'] + $_POST['question2'] + $_POST['question3'];
-        include('utilities/header.php');
-        showStarts($count);
-        include('utilities/footer.php');
-        foreach ($_POST as $key => $value) {
-            echo $key . '->' . $value . '<br>';
-        }
+    $count = $_POST['question1'] + $_POST['question2'] + $_POST['question3'];
+    include('utilities/header.php');
+    showStarts($count);
+    
+    if (isset($_POST['last_name']) && isset($_POST['first_name']) && isset($_POST['tel_number']) && isset($_POST['date_achat']) && isset($_POST['question1']) && isset($_POST['question2']) && isset($_POST['question3']) && isset($_POST['call_now']) && $_POST['question3'] == '-1' && $_POST['call_now'] == 'yes') {
+        contactUser($_POST['tel_number']);
     }
 
-    if (isset($_POST['last_name']) && isset($_POST['first_name']) && isset($_POST['tel_number']) && isset($_POST['date_achat']) && isset($_POST['question1']) && isset($_POST['question2']) && isset($_POST['question3']) && isset($_POST['call_now']) && $_POST['question3'] == '-1' && $_POST['call_now'] == 'yes') {
-        include('utilities/header.php');
-        contactUser($_POST['tel_number']);
-        include('utilities/footer.php');
-    } else {
-        $count = $_POST['question1'] + $_POST['question2'] + $_POST['question3'];
-        include('utilities/header.php');
-        showStarts($count);
-        include('utilities/footer.php');
-    }
+    include('utilities/footer.php');
 }
 
 ?>
--- ProjectForm/modele/functions.php
+++ ProjectForm/modele/functions.php
@@ -11,18 +11,11 @@ function showStarts(int $count): void
     for ($i = 0; $i < 5 - $count; $i++) {
         echo '⚫';
     }
-    echo '</p>
-          <p class="mt-5">
-              <a href="?" role="button" class="btn btn-danger">Recommencer</a>
-          </p>';
 }
 
 function contactUser(string $tel): void
 {
     echo '        
         <p>Vous serez rappelé au numéro '. $tel . '</p>
-        <p class="mt-5">
-              <a href="index.php" role="button" class="btn btn-danger">Retourner au questionaire</a>
-        </p>
     ';
 }
