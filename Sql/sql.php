96ee1b43c0c77adf2c67fda6fedcca4a45cdb6c9
democvidev <cnvvc_vlad@mail.ru>
2021-05-31 14:37:18 +0200

    updated query

--- Sql/sql.php
+++ Sql/sql.php
@@ -3,9 +3,10 @@
 $user = "root";
 $pass = "";
 $dbname = "18.05_wordpress_bd";
+$host = 'localhost';
 
 try {
-    $dsn = 'mysql:host=localhost;dbname=' . $dbname;
+    $dsn = 'mysql:host=' . $host . ';dbname=' . $dbname;
     $options = [
         PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
@@ -20,14 +21,18 @@ try {
 }
 
 try {
-    $dbh = new PDO('mysql:host=localhost;dbname=18.05_wordpress_bd', $user, $pass);
+    // $dbh = new PDO('mysql:host=localhost;dbname=18.05_wordpress_bd', $user, $pass);
     // var_dump($dbh);
-
-    $tab = $dbh->query('SELECT post_title, LEFT(post_content, 300), post_date 
+    $query = 'SELECT post_title, LEFT(post_content, 100) AS post_content_tr, post_date, display_name 
                         FROM wp_posts, wp_users
                         WHERE post_type="post"
-                            AND post_status="publish"');
-
+                            AND post_status="publish"
+                            AND post_author= wp_users.ID';
+    $req = $dbh->query($query);
+    $req->setFetchMode(PDO::FETCH_ASSOC);
+    $tab = $req->fetchAll();
+    $req->closeCursor();
+    // var_dump($tab);
     foreach ($tab as $row) {
         // print_r($row);
         var_dump($row);
