b79e431d1e9fc82a266a761d7c63956dffe5e6d1
democvidev <cnvvc_vlad@mail.ru>
2021-05-31 11:33:42 +0200

    add sql request

--- Sql/sql.php
+++ Sql/sql.php
@@ -5,8 +5,13 @@ $pass = "";
 
 try {
     $dbh = new PDO('mysql:host=localhost;dbname=18.05_wordpress_bd', $user, $pass);
-    var_dump($dbh);
-    $tab = $dbh->query('SELECT * from wp_posts');
+    // var_dump($dbh);
+
+    $tab = $dbh->query('SELECT post_title, LEFT(post_content, 30), post_date 
+                        FROM wp_posts
+                        WHERE post_type="post"
+                            AND post_status="publish"');
+
     foreach ( $tab as $row) {
         // print_r($row);
         var_dump($row);
