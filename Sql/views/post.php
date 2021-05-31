3c27b0f651271b8ac1a3de3c44ea58f6f8973b25
democvidev <cnvvc_vlad@mail.ru>
2021-05-31 22:43:55 +0200

    add previous button

--- Sql/views/post.php
+++ Sql/views/post.php
@@ -1,4 +1,7 @@
 <h1 class="d-flex justify-content-center"><?= $post['post_title'] ?></h1>        
 <p><?= $post['post_content'] ?></p>
 <p>Rédigé par : <?= $post['display_name'] ?> - Date : <?= $post['post_date'] ?></p>
+<div class="d-flex justify-content-end">
+    <a class="btn btn-success" href="index.php?">Retour aux articles</a>
+</div>
 
