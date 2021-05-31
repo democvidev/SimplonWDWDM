4e21e73825e823366de3cc712b5c8e6f42671178
democvidev <cnvvc_vlad@mail.ru>
2021-05-31 17:00:49 +0200

    show posts and add redirection

--- Sql/sql.php
+++ Sql/sql.php
@@ -12,7 +12,7 @@ try {
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ];
     $dbh = new PDO($dsn, $user, $pass, $options);
-    var_dump($dbh); 
+    // var_dump($dbh);
     
     echo 'Connexion établie !';
 } catch (PDOException $e) {
@@ -23,7 +23,7 @@ try {
 try {
     // $dbh = new PDO('mysql:host=localhost;dbname=18.05_wordpress_bd', $user, $pass);
     // var_dump($dbh);
-    $query = 'SELECT post_title, LEFT(post_content, 100) AS post_content_tr, post_date, display_name 
+    $query = 'SELECT wp_posts.ID, post_title, LEFT(post_content, 700) AS post_content_tr, post_date, display_name 
                         FROM wp_posts, wp_users
                         WHERE post_type="post"
                             AND post_status="publish"
@@ -33,14 +33,36 @@ try {
     $tab = $req->fetchAll();
     $req->closeCursor();
     // var_dump($tab);
-    foreach ($tab as $row) {
-        // print_r($row);
-        var_dump($row);
-    }
+    // Création html
+?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Titre de la page</title>
+    </head>
+    <body>
+        <h1>Accueil blog</h1>
+        <?php foreach($tab as $row) { ?>
+        <h2><a href="article.php?id=<?= $row['ID'] ?>"><?= $row['post_title'] ?></a></h2>
+        <p><?= $row['post_content_tr'] ?></p>
+        <p>Rédigé par : <?= $row['display_name'] ?> - Date : <?= $row['post_date'] ?></p>
+        <?php } ?>
+    </body>
+    </html>
+
+<?php
+    // var_dump($tab);
+    // foreach ($tab as $row) {
+    //     // print_r($row);
+    //     var_dump($row);
+    // }
     $dbh = null;
     echo 'Fin du SQL !';
 } catch (PDOException $e) {
     print "Erreur !: " . $e->getMessage() . "<br/>";
     die();
 }
-
